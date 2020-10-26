5f74e41e1dd9273ab7ac4412bc1965c5941c19bb
Chintan Kotian <chintankotian@gmail.com>
2020-10-26 19:17:01 +0530

    completed imageDataLoader

--- data/imageLoader.py
+++ data/imageLoader.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img, save_img
 import os
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -70,6 +70,38 @@ class datasetLoader():
         return data
 
     
+    def loadImage(self, imgPath, imgSize = None,applyTransform = True):
+        # loads single images and applies pixel transforms
+        if(not imgSize):
+            imgSize = self.imgSize
+
+        img = load_img(imgPath)
+        img = img_to_array(img)
+        if(applyTransform):
+            img = self.applyTransform(img * self.imgScaling)
+        
+        return img
+
+    
+    def saveImage(self, img, imgName, imgPath = "./imageLoaderImages"):
+        # Converts img array to img and saves in the given directory
+        if(not os.path.isdir(imgPath)):
+            os.mkdir(imgPath)
+        img = np.array(img)
+        imgMax = img.max()
+        imgMin = img.min()
+
+        if(imgMin < 0):
+            img = self.reverseTransform(img)
+        
+        if(imgMax > 1):
+            img = img * (1/self.imgScaling)
+        
+        savePath = os.path.join(imgPath,imgName)
+        save_img(savePath, img)
+
+
+    
 
 
 if(__name__ == "__main__"):
@@ -79,25 +111,25 @@ if(__name__ == "__main__"):
     print("Min = "+ str(batchImages.min()))
     print("max = "+ str(batchImages.max()))
 
-    plt.imshow(batchImages[0])
-    plt.show()
+    # plt.imshow(batchImages[0])
+    # plt.show()
 
-    print("*"*5)
+    # print("*"*5)
 
-    print("Standard Scaling")
-    loader.applyRescale1()
-    batchImages,_ = loader.nextBatch()
-    print("Min = "+ str(batchImages.min()))
-    print("max = "+ str(batchImages.max()))
+    # print("Standard Scaling")
+    # loader.applyRescale1()
+    # batchImages,_ = loader.nextBatch()
+    # print("Min = "+ str(batchImages.min()))
+    # print("max = "+ str(batchImages.max()))
 
-    print("*"*5)
-    print("Reverse transform function")
-    newData = loader.reverseTransform(batchImages)
-    print("Min = "+str(newData.min()))
-    print("Max = "+str(newData.max()))
+    # print("*"*5)
+    # print("Reverse transform function")
+    # newData = loader.reverseTransform(batchImages)
+    # print("Min = "+str(newData.min()))
+    # print("Max = "+str(newData.max()))
 
-    plt.imshow(newData[0])
-    plt.show()
+    # plt.imshow(newData[0])
+    # plt.show()
 
     # print("Batch imdex at start = "+str(loader.getBatchIndex()))
     # reset  = False
@@ -107,3 +139,37 @@ if(__name__ == "__main__"):
         
     # print("-"*10)
     # print("Batch imdex at end = "+str(loader.getBatchIndex()))
+
+    print("*"*5)
+    print("Testing loading and saving images")
+
+    print("1 - ")
+    print('Loading images without scaling')
+    path = os.path.join("../dataset/img_align_celeba/img_align_celeba",os.listdir("../dataset/img_align_celeba/img_align_celeba")[0] )
+    unscaledImg = loader.loadImage(path, applyTransform=False)
+    print("Unscaled image max = ",unscaledImg.max())
+    print("Unscaled image min = ",unscaledImg.min())
+
+    print("2 - ")
+    print('Loading images with scaling')
+    # path = os.path.join("./datset/img_align_celeba/img_align_celeba",os.listdir("./datset/img_align_celeba/img_align_celeba")[0] )
+    scaledImg = loader.loadImage(path, applyTransform=True)
+    print("scaled image max = ",scaledImg.max())
+    print("scaled image min = ",scaledImg.min())
+
+    print("3 - ")
+    print('Loading images with normal scaling')
+    # path = os.path.join("./datset/img_align_celeba/img_align_celeba",os.listdir("./datset/img_align_celeba/img_align_celeba")[0] )
+    loader.applyRescale1(switch=True)
+    normalScaledImg = loader.loadImage(path, applyTransform=True)
+    print("normalScaled image max = ",normalScaledImg.max())
+    print("normalScaled image min = ",normalScaledImg.min())
+
+    print("Saving all kinds of images to default directory")
+    loader.saveImage(img=unscaledImg,imgName="unscaledImg.jpg")
+    print('Unscaled img saved')
+    loader.saveImage(img=scaledImg,imgName="scaledImg.jpg")
+    print('scaledImg img saved')
+    loader.saveImage(img=normalScaledImg,imgName="normalScaledImg.jpg")
+    print('normalScaledImg img saved')
+    
